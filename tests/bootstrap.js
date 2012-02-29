b4f8bc3ccf69f8ba958078578a8b132b62f4666c
Leon Sorokin <leeoniya@gmail.com>
2012-02-29 00:50:35 -0600

    some test prepping

---
+++ tests/bootstrap.js
@@ -0,0 +1,18 @@
+var list = [
+	"reMarked.js",
+	"AACL",
+	"aXMLRPC",
+	"chaplin",
+	"evandrix.github.com",
+	"github_flavored",
+	"glossy-rails",
+	"jquery-pjax",
+	"jRumble",
+	"jQuery",
+	"kohanafacebook",
+	"marked",
+	"PEG.js",
+	"Plate.js",
+	"Resource-Tree",
+	"TaskList"
+];
